84d99ef98339676e0f6925c98157e5f12f45e27f
Vinicius Menezes <vini.ads13@outlook.com>
2022-03-10 21:50:19 -0300

    :pencil: add the program logic and changing HTML to EJS, linking server too and i did the route page

---
+++ public/scripts/main.js
@@ -0,0 +1,35 @@
+import Modal from './modal.js'
+
+const modal = Modal()
+
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
+//Pegar todos os botões que existe com a classe check
+const checkButtons = document.querySelectorAll('.actions a.check')
+
+checkButtons.forEach(button => {
+  button.addEventListener('click', handleClick)
+})
+
+// Quando o botão delete for clicado ele abre a modal
+const deleteButton = document.querySelectorAll('.actions a.delete')
+
+deleteButton.forEach(button => {
+  button.addEventListener('click', event => handleClick(event, false))
+})
+
+function handleClick(event, check = true) {
+  event.preventDefault()
+  const text = check ? 'Marcar como lido' : 'Excluir'
+
+  modalTitle.innerHTML = `${text} esta pergunta`
+  modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} sua pergunta ?`
+
+  modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
+  check ? modalButton.classList.remove('red') : modalButton.classList.add('red')
+
+  //abrir modal
+  modal.open()
+}
