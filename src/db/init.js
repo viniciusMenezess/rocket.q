08c676d89fd0d07e55011b7076e7c27d2b308cbe
Vinicius Menezes <vini.ads13@outlook.com>
2022-03-14 10:56:26 -0300

    :tada:final project version

--- src/db/init.js
+++ src/db/init.js
@@ -1,22 +1,27 @@
 const Database = require("./config")
 
 const initDb = {
-  async init() {
-    const db = await Database()
+    async init(){
+        const db = await Database()
 
-    await db.exec(`CREATE TABLE rooms(
+        await db.exec(`CREATE TABLE rooms (
             id INTEGER PRIMARY KEY,
             pass TEXT
         )`);
 
-    await db.exec(`CREATE TABLE questions (
+        await db.exec(`CREATE TABLE questions (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             title TEXT,
-            read INT
+            read INT,
+            room INT
         )`);
 
-    await db.close()
-  }
+        await db.close()
+    }
 }
 
-initDb.init()
+initDb.init();
+
+
+
+
